b8bf15798ce193bca1906540791a096d442c4f40
Jonathan Garza <83787061+jonathanrgarza@users.noreply.github.com>
2024-05-23 02:09:47 -0500

    Add ICloseable.cs and ICloseableDialog.cs to allow view models to request closing of their windows

---
+++ Ncl.Common.Wpf/Infrastructure/ICloseable.cs
@@ -0,0 +1,12 @@
+﻿namespace Ncl.Common.Wpf.Infrastructure;
+
+/// <summary>
+/// Represents a closeable object/window.
+/// </summary>
+public interface ICloseable
+{
+    /// <summary>
+    /// Requests the object/window to close.
+    /// </summary>
+    void Close();
+}
---
+++ Ncl.Common.Wpf/Infrastructure/ICloseableDialog.cs
@@ -0,0 +1,13 @@
+﻿namespace Ncl.Common.Wpf.Infrastructure;
+
+/// <summary>
+/// Interface for a dialog that can be closed.
+/// </summary>
+public interface ICloseableDialog : ICloseable
+{
+    /// <summary>
+    /// Closes the dialog with the specified dialog result.
+    /// </summary>
+    /// <param name="dialogResult">The dialog result to set.</param>
+    void CloseDialog(bool? dialogResult = null);
+}
